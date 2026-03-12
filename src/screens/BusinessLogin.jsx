e3b2eda0e001ede95fb765aac97dd6f1655b59e4
User <user@example.com>
2026-03-12 22:43:23 +1300

    removed window logic

--- src/screens/BusinessLogin.jsx
+++ src/screens/BusinessLogin.jsx
@@ -150,9 +150,7 @@ export default function BusinessLogin() {
               gdpr_consent_date: new Date().toISOString(),
               referral_code: referralCode // Store referral code for later use
             },
-            emailRedirectTo: process.env.NODE_ENV === 'production' 
-              ? `${window.location.origin}/BusinessLogin`
-              : `${process.env.NEXT_PUBLIC_APP_PROD_URL}/BusinessLogin`
+            emailRedirectTo: `${process.env.NEXT_PUBLIC_APP_PROD_URL}/BusinessLogin`
           }
         });
       }
